0a92166ac0d7ac8f6e656b05b0bc1b1315318c7a
Erik Anstine <erikanstine@gmail.com>
2022-08-18 16:51:08 -0400

    add round page

--- src/pages/rounds/Rounds.jsx
+++ src/pages/rounds/Rounds.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import './rounds.css';
+import Sidebar from '../../components/sidebar/Sidebar';
 import Topbar from '../../components/topbar/Topbar';
+import Round from '../../components/round/Round';
+
+import { Rounds as dummyRounds } from '../../dummyDiscData';
 
 export default function Rounds() {
-    return (
-        <>
-        <Topbar />
-        Rounds page!
-        </>
-    )
+  return (
+    <>
+      <Topbar />
+      <div className='roundsContainer'>
+        <Sidebar submenu='rounds' />
+        <div className='roundsCenter'>
+          <div className='roundsTitle'>Your rounds</div>
+          <div className='roundsListContainer'>
+            {dummyRounds.map((r) => (
+              <Round key={r.id} round={r} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
